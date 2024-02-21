2da346217e50e0c8d2697f7b4a4eb6fd3e51caa7
tsnicholas <tsnicholas@bsu.edu>
2024-02-21 17:16:51 -0500

    Add events to each button

--- js/Project1.js
+++ js/Project1.js
@@ -1,3 +1,25 @@
-const nodeModel = require("./nodeModel");
-const stack = new nodeModel.StackNode();
+import $ from "jQuery";
+import { StackNode } from "./nodeModel";
 
+const stack = new StackNode();
+
+$("#pushButton").on("click", () => {
+    const input = $("inputNumber").val();
+    stack.push(input);
+});
+
+$("#popButton").on("click", () => {
+    stack.pop();
+});
+
+$("#peekButton").on("click", () => {
+    stack.peek();
+});
+
+$("#sizeButton").on("click", () => {
+    console.log(stack.size);
+});
+
+$("#emptyButton").on("click", () => {
+    console.log(isEmpty());
+});
